7e602b7fd7355fe189ec6cb0b506d9a5ecc97f77
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-06-09 20:06:26 +0530

    Error handling and readme added

---
+++ Readme.md
@@ -0,0 +1,12 @@
+- Navigate into the project directory
+- Add api key to .env
+- Install required modules
+
+   ```bash
+   $ npm install
+   ```
+- Run the app
+
+   ```bash
+    node generate.js
+   ```
--- generate.js
+++ generate.js
@@ -10,6 +10,11 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+if (!configuration.apiKey) {
+  console.log("OpenAI API key not configured");
+  process.exit(1);
+}
+
 const userInterface = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -17,11 +22,19 @@ const userInterface = readline.createInterface({
 
 userInterface.prompt()
 userInterface.on("line", async input => {
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [{ role: "user", content: input }],
-  })
-  console.log(response.data.choices[0].message.content);
+  try {
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: "user", content: input }],
+    })
+    console.log(response.data.choices[0].message.content);
+  } catch (error) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+    }
+  }
 })
 
 
